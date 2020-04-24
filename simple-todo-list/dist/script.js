cbbaf5ce82a8a9607cef88c6a7466ad49b3cc570
traplda <traplda@vscht.cz>
2020-04-24 18:40:06 +0200

    some plots

--- simple-todo-list/dist/script.js
+++ simple-todo-list/dist/script.js
@@ -1,14 +1,14 @@
-$('button').click(() => {
+/* $('button').click(() => {
   $('ul').append(`<li>${$('input').val()}</li>`);
   $('input').val('');
-});
+}); */
 
 // Extra version with completing todos
 
-// $('button').click(() => {
-//   $('ul').append(`<li>${$('input').val()}</li>`);
-//   $('li:last-child').click((event) => {
-//     $(event.target).toggleClass('completed');
-//   });
-//   $('input').val('');
-// });
+$('button').click(() => {
+  $('ul').append(`<li>${$('input').val()}</li>`);
+  $('li:last-child').click((event) => {
+    $(event.target).toggleClass('completed');
+  });
+  $('input').val('');
+});
