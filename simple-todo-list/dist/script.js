73e13aa4ece52a9e5acba2df194eeeb77fdd67a3
traplda <traplda@vscht.cz>
2020-04-24 20:17:44 +0200

    new version

--- simple-todo-list/dist/script.js
+++ simple-todo-list/dist/script.js
@@ -6,7 +6,10 @@
 // Extra version with completing todos
 
 $('button').click(() => {
-  $('ul').append(`<li>${$('input').val()}</li>`);
+  $('ul').append(`<dev class="new-item"></dev>`);
+  $('.new-item:last-child').append(`<li>${$('input').val()}</li>`);
+  $('.new-item:last-child').append(`<img src="https://img.icons8.com/officel/30/000000/delete.png"/>`);
+  $('.new-item:last-child').append(`<img id="done" src="https://img.icons8.com/dotty/80/000000/checkmark.png"/>`);
   $('li:last-child').click((event) => {
     $(event.target).toggleClass('completed');
   });
