40fe32b26c8a1006992ca14d2a7f6a482ca65a69
traplda <traplda@vscht.cz>
2020-04-26 11:25:15 +0200

    changing photos

--- paraguay/scripts.js
+++ paraguay/scripts.js
@@ -1,6 +1,36 @@
 let i=0
 
+let photos = ['img1.png','img2.png','img3.png']
+
+$('.right').click(function() {
+  if (i < 2) {
+    i++
+    $('.left').css('background-color','white')
+    console.log(i)
+    $('.photo-large').children().replaceWith('<img src="img/' + photos[i] +'" alt="photo">')
+    if (i == 2) {
+      $('.right').css('background-color','silver')
+    }
+
+  }  
+})
+
+$('.left').click(function() {
+  if (i > 0) {
+    i--
+    $('.right').css('background-color','white')
+    console.log(i)
+    $('.photo-large').children().replaceWith('<img src="img/' + photos[i] +'" alt="photo">')
+    if (i == 0) {
+      $('.left').css('background-color','silver')
+    }
+
+  }  
+})
+
 if (i == 0) {
   $('.left').css('background-color','silver')
   $('.min1').css('border','solid black 1px')
 }
+
+$('.photo-large').append('<img src="img/' + photos[i] +'" alt="photo">')
